c04f24cd7c190dd55c2dfeefd3c27c0b45a21c36
Tilacin <tilacin45@gmail.com>
2022-10-12 21:11:32 +0300

    2 task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2 ) {
+  let arr1 = s1.split('')
+  arr1 = new Set(arr1)
+  let arr2 = s2.split('')
+ arr2 = new Set(arr2)
+ var count = arr1.filter(x => arr2.indexOf(x) !== -1)
+    
+  return count.length
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!(members instanceof Array))
+  return false
+  let result = []
+  for(let i = 0; i < members.length; i++) {
+    if(typeof members[i] === 'string') {
+      const trimed = members[i].trim()
+      result.push(trimed[0].toUpperCase())
+    }
+  }
+  return result.sort().join("")
 }
 
 module.exports = {
